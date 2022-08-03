338a3d37650b51dae7979f182ccec79226c8dae2
Thorben Stangenberg <thorben.stangenberg@42talents.com>
2022-08-03 15:43:32 +0200

    initial spring project

---
+++ src/main/java/com/_42talents/starter_spring_karate/StarterSpringKarateApplication.java
@@ -0,0 +1,13 @@
+package com._42talents.starter_spring_karate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterSpringKarateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterSpringKarateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_42talents/starter_spring_karate/StarterSpringKarateApplicationTests.java
@@ -0,0 +1,13 @@
+package com._42talents.starter_spring_karate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterSpringKarateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
