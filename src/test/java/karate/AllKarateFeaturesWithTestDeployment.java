6a49fad3417bb19ebb8d377399bb353318eae7ae
Thorben Stangenberg <thorben.stangenberg@42talents.com>
2022-08-07 14:56:51 +0200

    karate test with heroku deployment

--- src/test/java/karate/AllKarateFeaturesWithTestDeployment.java
+++ src/test/java/karate/AllKarateFeaturesWithTestDeployment.java
@@ -21,25 +21,16 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. */
 package karate;
 
-import com._42talents.spring_boot_karate_example.SpringBootKarateExampleApplication;
 import com.intuit.karate.Results;
 import com.intuit.karate.junit5.Karate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest(
-    classes = {SpringBootKarateExampleApplication.class},
-    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class AllKarateFeatures {
-
-  @LocalServerPort private String localServerPort;
+public class AllKarateFeaturesWithTestDeployment {
 
   @Test
   public void runAllFeaturesInParallel() {
-    Results results =
-        Karate.run(".").systemProperty("karate.port", localServerPort).karateEnv("dev").parallel(3);
+    Results results = Karate.run("./target/test-classes/karate").karateEnv("test").parallel(8);
     Assertions.assertEquals(0, results.getFailCount(), results.getErrorMessages());
   }
 }
--- src/test/resources/karate-config.js
+++ src/test/resources/karate-config.js
@@ -5,12 +5,18 @@ function fn() {
         env = 'dev'; // a custom 'intelligent' default
         karate.log('karate.env set to "dev" as default.');
     }
-    var config = '';
-    var port = karate.properties['karate.port'] || '8080'
-    if (env === 'dev') {
+    let config;
+    if (env === 'test') {
+        config = {
+            baseUrl: 'https://spring-boot-karate-example.herokuapp.com'
+        }
+    } else if (env === 'dev') {
+        let port = karate.properties['karate.port'] || '8080'
         config = {
             baseUrl: 'http://localhost:' + port
         }
+    } else {
+        throw 'Unknown environment [' + env + '].'
     }
     // don't waste time waiting for a connection or if servers don't respond within 0,3 seconds
     karate.configure('connectTimeout', 300);
